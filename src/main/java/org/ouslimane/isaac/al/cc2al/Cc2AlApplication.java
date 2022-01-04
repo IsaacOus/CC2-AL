50c48021b33ffa25445f6b5a3d571b38a8e592cd
IsaacOus <iouslimane@myges.fr>
2022-01-04 08:21:02 +0100

    Initial commit

---
+++ src/main/java/org/ouslimane/isaac/al/cc2al/Cc2AlApplication.java
@@ -0,0 +1,13 @@
+package org.ouslimane.isaac.al.cc2al;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cc2AlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cc2AlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ouslimane/isaac/al/cc2al/Cc2AlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ouslimane.isaac.al.cc2al;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cc2AlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
